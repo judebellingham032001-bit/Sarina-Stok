32e5659da9701ffd664270be7d7240a2fc9a73b5
judebellinghamcls <judebellingham032001@gmail.com>
2026-08-03 05:56:32 +0700

    fix: next config typescript types

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,6 @@ const nextConfig: NextConfig = {
     // Abaikan error TypeScript saat build
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // Abaikan error ESLint saat build
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
